cdd62666b9da8e2690f8d5f5211e9ca4a51fdcc8
SimeraHailu <68601178+SimeraHailu@users.noreply.github.com>
2021-12-09 09:52:21 -0600

    Update EvaluationsController.cs

--- admin/Controller/EvaluationsController.cs
+++ admin/Controller/EvaluationsController.cs
@@ -15,40 +15,41 @@ using Epa.Camd.Quartz.Scheduler.Logging;
 
 namespace Epa.Camd.Quartz.Scheduler
 {
-  [ApiController]
-  [Route("quartz/api/triggers/evaluations")]
-  [Produces("application/json")]
-  public class EvaluationsController : ControllerBase
-  {
-    private readonly ILogger _logger;
-    private NpgSqlContext _dbContext = null;
-    private IConfiguration Configuration { get; }
-
-    public EvaluationsController(
-      NpgSqlContext dbContext,
-      IConfiguration configuration,
-      ILogger<CheckEngineEvaluation> logger
-    )
+    [ApiController]
+    [Route("quartz/api/triggers/evaluations")]
+    [Produces("application/json")]
+    public class EvaluationsController : ControllerBase
     {
-      _logger = logger;
-      _dbContext = dbContext;
-      Configuration = configuration;
-    }
+        private readonly ILogger _logger;
+        private NpgSqlContext _dbContext = null;
+        private IConfiguration Configuration { get; }
 
-    private async Task<ActionResult> TriggerCheckEngineEvaluation(
-      string processCode,
-      string monitorPlanId,
-      string userId,
-      string userEmail
-    ) {
-      Services services = (Services)Request.HttpContext.Items[typeof(Services)];
+        public EvaluationsController(
+          NpgSqlContext dbContext,
+          IConfiguration configuration,
+          ILogger<CheckEngineEvaluation> logger
+        )
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            Configuration = configuration;
+        }
 
-      Guid id = Guid.NewGuid();
-      DateTime submittedOn = DateTime.Now;
+        private async Task<ActionResult> TriggerCheckEngineEvaluation(
+          string processCode,
+          string monitorPlanId,
+          string userId,
+          string userEmail
+        )
+        {
+            Services services = (Services)Request.HttpContext.Items[typeof(Services)];
 
-      MonitorPlan mp = _dbContext.MonitorPlans.Find(monitorPlanId);
-      Facility fac = _dbContext.Facilities.Find(mp.FacilityId);
-      List<MonitorLocation> locs = _dbContext.MonitorLocations.FromSqlRaw(@"
+            Guid id = Guid.NewGuid();
+            DateTime submittedOn = DateTime.Now;
+
+            MonitorPlan mp = _dbContext.MonitorPlans.Find(monitorPlanId);
+            Facility fac = _dbContext.Facilities.Find(mp.FacilityId);
+            List<MonitorLocation> locs = _dbContext.MonitorLocations.FromSqlRaw(@"
           SELECT ml.mon_loc_id, u.unit_id, u.unitid, sp.stack_pipe_id, sp.stack_name
           FROM camdecmpswks.monitor_location ml
           JOIN camdecmpswks.monitor_plan_location USING(mon_loc_id)
@@ -56,65 +57,65 @@ namespace Epa.Camd.Quartz.Scheduler
           LEFT JOIN camdecmpswks.stack_pipe sp ON ml.stack_pipe_id = sp.stack_pipe_id
           WHERE mon_plan_id = {0}
           ORDER BY unitId, stack_name",
-        monitorPlanId
-      ).ToList();
+              monitorPlanId
+            ).ToList();
 
-      string monPlanConfig = string.Join(", ", locs.Select(x =>
-        string.IsNullOrWhiteSpace(x.UnitName) ? x.StackName : x.UnitName
-      ));
+            string monPlanConfig = string.Join(", ", locs.Select(x =>
+              string.IsNullOrWhiteSpace(x.UnitName) ? x.StackName : x.UnitName
+            ));
 
-      mp.EvalStatus = "INQ"; // set eval status to In Queue
-      _dbContext.MonitorPlans.Update(mp);
-      _dbContext.SaveChanges();
+            await CheckEngineEvaluation.StartNow(
+              services.Scheduler,
+              id,
+              processCode,
+              fac.OrisCode,
+              fac.Name,
+              monitorPlanId,
+              monPlanConfig,
+              userId,
+              userEmail,
+              submittedOn
+            );
 
-      await CheckEngineEvaluation.StartNow(
-        services.Scheduler,
-        id,
-        processCode,
-        fac.OrisCode,
-        fac.Name,
-        monitorPlanId,
-        monPlanConfig,
-        userId,
-        userEmail,
-        submittedOn
-      );
+            mp.EvalStatus = "INQ"; // set eval status to In Queue
+            _dbContext.MonitorPlans.Update(mp);
+            _dbContext.SaveChanges();
 
-      return CreatedAtAction("EvaluationResponse)", new
-      {
-        id = id,
-        processCode = processCode,
-        facilityId = fac.OrisCode,
-        facilityName = fac.Name,
-        monitorPlanId = monitorPlanId,
-        configuration = monPlanConfig,
-        userId = userId,
-        userEmail = userEmail,
-        submittedOn = submittedOn
-      });
+            return CreatedAtAction("EvaluationResponse)", new
+            {
+                id = id,
+                processCode = processCode,
+                facilityId = fac.OrisCode,
+                facilityName = fac.Name,
+                monitorPlanId = monitorPlanId,
+                configuration = monPlanConfig,
+                userId = userId,
+                userEmail = userEmail,
+                submittedOn = submittedOn
+            });
+        }
+
+        [HttpPost("monitor-plans")]
+        public async Task<ActionResult> TriggerMPEvaluation([FromBody] EvaluationRequest request)
+        {
+            //string apiKey = Request.Headers["x-api-key"];
+            //string allowedKeys = Configuration["EASEY_QUARTZ_SCHEDULER_EVALUATIONS_API_KEYS"];
+
+            //if (apiKey != null && allowedKeys.Split(',').Contains(apiKey))
+            {
+                return await TriggerCheckEngineEvaluation(
+                  "MP",
+                  request.MonitorPlanId,
+                  request.UserId,
+                  request.UserEmail
+                );
+            }
+            //else
+            //{
+            //  string message = "API Key is either missing or not an authorized client!";
+            //  LogHelper.error(_logger, message, new LogVariable("API Key", apiKey), new LogVariable("Request", request));
+            //  return BadRequest(message);
+            //}
+        }
     }
-
-    [HttpPost("monitor-plans")]
-    public async Task<ActionResult> TriggerMPEvaluation([FromBody] EvaluationRequest request)
-    {
-      string apiKey = Request.Headers["x-api-key"];
-      string allowedKeys = Configuration["EASEY_QUARTZ_SCHEDULER_EVALUATIONS_API_KEYS"];
-
-      if (apiKey != null && allowedKeys.Split(',').Contains(apiKey))
-      {
-        return await TriggerCheckEngineEvaluation(
-          "MP",
-          request.MonitorPlanId,
-          request.UserId,
-          request.UserEmail
-        );
-      }
-      else
-      {
-        string message = "API Key is either missing or not an authorized client!";
-        LogHelper.error(_logger, message, new LogVariable("API Key", apiKey), new LogVariable("Request", request));
-        return BadRequest(message);
-      }
-    }
-  }
 }
