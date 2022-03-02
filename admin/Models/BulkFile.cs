023c933689aa14bd31ec73b27ae83397350fa453
Jason Whitehead <jwhitehead@austin.rr.com>
2022-03-02 11:32:41 -0600

    added bulk file controller to return list of bulk files available in s3

---
+++ admin/Models/BulkFile.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Epa.Camd.Quartz.Scheduler.Models
+{
+	public class BulkFile {
+		public string Filename { get; set; }
+		public string S3Path { get; set; }
+		public string DataType { get; set; }
+		public string DataSubType { get; set; }
+		public string Grouping { get; set; }
+		public long Bytes { get; set; }
+		public long KiloBytes { get { return this.Bytes / 1024; } }
+		public long MegaBytes { get { return this.KiloBytes / 1024; } }
+		public long GigaBytes { get { return this.MegaBytes / 1024; } }		
+		public DateTime LastUpdated { get; set; }
+	}
+}
