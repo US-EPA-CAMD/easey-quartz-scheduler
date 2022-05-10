68091b9227d229e62a55739ce7dc262927ea99f2
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-05-09 20:41:19 -0400

    Removed commented code

--- admin/Jobs/BulkDataFiles/ApportionedEmissionsBulkDataFiles.cs
+++ admin/Jobs/BulkDataFiles/ApportionedEmissionsBulkDataFiles.cs
@@ -52,7 +52,7 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
     public async Task Execute(IJobExecutionContext context)
     {
       
-      /*
+      
       // Does this job already exist? Otherwise create and schedule a new copy
       List<List<Object>> jobAlreadyExists = await _dbContext.ExecuteSqlQuery("SELECT * FROM camdaux.job_log WHERE job_name = 'Apportioned Emissions' AND add_date::date = now()::date;", 9);
       if(jobAlreadyExists.Count != 0){
@@ -66,7 +66,7 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
           return;
         }
       }
-      */
+      
 
       LogHelper.info("Executing ApportionedEmissionsBulkDataFiles job");
 
