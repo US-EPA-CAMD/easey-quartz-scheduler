cb88fe8476957aede043afb219623caabd22869b
SimeraHailu <68601178+SimeraHailu@users.noreply.github.com>
2023-04-24 11:19:21 -0500

    Update Process.cs

--- CheckEngine/CheckEngine/Process.cs
+++ CheckEngine/CheckEngine/Process.cs
@@ -1272,12 +1272,19 @@ namespace ECMPS.Checks.CheckEngine
             {
                 Sql = "select camdecmpswks.update_ecmps_status_for_qa_evaluation('" + CheckEngine.ChkSessionId + "','" + QaId + "')";
             }
-            else if (CheckEngine.ProcessCd == "HOURLY") {
-                Sql = "select camdecmpswks.update_ecmps_status_for_em_evaluation('" + CheckEngine.ChkSessionId +  "','" + CheckEngine.RptPeriodId + "')";
+            else if (CheckEngine.ProcessCd == "HOURLY")
+            {
+                Sql = "select camdecmpswks.update_ecmps_status_for_em_evaluation('" + CheckEngine.ChkSessionId + "','" + CheckEngine.RptPeriodId + "')";
             }
             else if (CheckEngine.ProcessCd == "MP")
             {
                 Sql = "select camdecmpswks.update_ecmps_status_for_mp_evaluation('" + CheckEngine.MonPlanId + "','" + CheckEngine.ChkSessionId + "')";
+            }
+            else { 
+
+              errorMessage = string.Format(resultTemplate, "DB/App", "process code is not found");
+               Sql = "";
+               return false;
             }
 
             if (DbUpdate_EcmpsStatusProcess != null)
