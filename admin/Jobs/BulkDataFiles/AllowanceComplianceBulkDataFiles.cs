577f36095f26b0b03892e0aca2bf902cd24d0e4a
ergjustin <justin.joseph.erg.com>
2024-02-13 08:51:31 -0600

    rowsperprg line change

--- admin/Jobs/BulkDataFiles/AllowanceComplianceBulkDataFiles.cs
+++ admin/Jobs/BulkDataFiles/AllowanceComplianceBulkDataFiles.cs
@@ -90,6 +90,8 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
       try
       {
 
+        List<List<Object>> rowsPerPrg = await _dbContext.ExecuteSqlQuery("SELECT * FROM camdaux.vw_allowance_based_compliance_bulk_files_to_generate", 1);
+
         jl.JobId = job_id;
         jl.JobSystem = "Quartz";
         jl.JobClass = "Bulk Data File";
@@ -101,9 +103,6 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
 
         _dbContext.JobLogs.Add(jl);
         await _dbContext.SaveChangesAsync();
-        
-        List<List<Object>> rowsPerPrg = await _dbContext.ExecuteSqlQuery("SELECT * FROM camdaux.vw_allowance_based_compliance_bulk_files_to_generate", 1);
-        
         
         for(int row = 0; row < rowsPerPrg.Count; row++){
           string code = (string) rowsPerPrg[row][0];
