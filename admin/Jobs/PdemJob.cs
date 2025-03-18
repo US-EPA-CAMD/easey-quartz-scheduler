a9353ce83f98fa97a98d1ed58e31424a048ce8b3
DWhitten <Dwayne.Whitten@erg.com>
2025-03-18 10:39:13 -0400

    #6603 Addition of the PDEM job including the PdemReport model.

---
+++ admin/Jobs/PdemJob.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using Quartz;
+
+using Epa.Camd.Quartz.Scheduler.Models;
+
+using DatabaseAccess;
+using ECMPS.DM;
+
+
+namespace Epa.Camd.Quartz.Scheduler.Jobs
+{
+
+    public class PdemJob : IJob
+    {
+
+        public PdemJob(NpgSqlContext dbContext, IConfiguration configuration, ILogger<PdemJob> logger)
+        {
+            _dbContext = dbContext;
+            Configuration = configuration;
+            _logger = logger;
+        }
+
+        public IConfiguration Configuration { get; }
+        private NpgSqlContext _dbContext = null;
+        private readonly ILogger<PdemJob> _logger;
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            string instanceIndex = Environment.GetEnvironmentVariable("CF_INSTANCE_INDEX") ?? "unknown";
+            int maxAllowed = Int32.Parse(Configuration["EASEY_QUARTZ_SCHEDULER_MAX_PDEM"] ?? "3");  //TODO: Need to create/handle EASEY_QUARTZ_SCHEDULER_MAX_PDEM
+
+            try
+            {
+                for (int dex = 1; dex <= maxAllowed; dex++)
+                {
+                    List<PdemReport> pdemReportList = _dbContext.PdemReport.FromSql($"select pdem_report_id, mon_plan_id, rpt_period_id, submission_id from camdecmpsaux.PDEM_Job_Get_Next({maxAllowed})").ToList();
+
+                    if (pdemReportList.Count > 0)
+                    {
+                        cUpdateEmissions updateEmissions = new(ConnectionStringManager.getConnectionString(Configuration), _logger, 20);
+
+                        updateEmissions.ProcessEmissionReport(pdemReportList[0].PdemReportId);
+                    }
+                }
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("[Instance {InstanceIndex}] Error in evaluation queue: {ErrorMessage}", instanceIndex, ex.Message);
+                return Task.FromException(ex);
+            }
+        }
+
+    }
+
+}
--- admin/Models/NpgSqlContext.cs
+++ admin/Models/NpgSqlContext.cs
@@ -11,241 +11,243 @@ using NpgsqlTypes;
 
 namespace Epa.Camd.Quartz.Scheduler.Models
 {
-  public class NpgSqlContext : DbContext
-  {
-    private readonly ILogger _logger;
-    public IConfiguration Configuration { get; }
-
-    public DbSet<BulkFileMetadata> BulkFileMetadataSet { get; set; }
-    public DbSet<BulkFileQueue> BulkFileQueue { get; set; }
-    public DbSet<JobConfiguration> JobConfigurations { get; set; }
-    public DbSet<JobLog> JobLogs { get; set; }
-    public DbSet<ReportingPeriod> ReportingPeriods { get; set; }
-    public DbSet<BulkFileLog> BulkFileLogs { get; set; }
-    public DbSet<InventoryStatusLog> InventoryStatusLogs { get; set; }
-
-    public DbSet<SubmissionSet> SubmissionSet { get; set; }
-
-    public DbSet<EmailToProcess> EmailToProcessQueue { get; set; }
-    public DbSet<EmailToSend> EmailToSend { get; set; }
-    public DbSet<ProgramCode> ProgramCodes { get; set; }
-    //public DbSet<EmissionEvaluation> EmissionEvaluations { get; set; } 
-    public DbSet<EvaluationSet> EvaluationSet { get; set; }
-    public DbSet<Evaluation> Evaluations { get; set; }
-    public DbSet<SeverityCode> SeverityCodes { get; set; }
-    public DbSet<EmissionEvaluation> EmissionEvaluations { get; set; }
-    public DbSet<EvalStatusCode> EvalStatusCodes { get; set; }
-    public DbSet<CheckSession> CheckSessions { get; set; }
-    public DbSet<CorsOptions> CorsOptions { get; set; }
-    public DbSet<Facility> Facilities { get; set; }
-    public DbSet<MonitorPlan> MonitorPlans { get; set; }
-
-    public DbSet<CertEvent> CertEvents { get; set; }
-
-    public DbSet<TestSummary> TestSummaries { get; set; }
-
-    public DbSet<TestExtensionExemption> TestExtensionExemptions { get; set; }
-    public DbSet<MonitorLocation> MonitorLocations { get; set; }
-    public DbSet<MonitorPlanLocation> MonitorPlanLocations { get; set; }
-
-    public NpgSqlContext(IConfiguration configuration, ILogger<NpgSqlContext> logger, DbContextOptions<NpgSqlContext> options) : base(options)
+    public class NpgSqlContext : DbContext
     {
-      _logger = logger;
-      Configuration = configuration;
-    }
-    public async Task CreateBulkFileRecord(string name, Guid parent_id, int? year, int? quarter, string stateCd, string dataType, string subType, string url, string fileName, Guid job_id, string program_code)
-    {
-      try
-      {
-        Guid child_job_id = Guid.NewGuid();
-        BulkFileQueue jl = new BulkFileQueue();
-        jl.JobId = child_job_id;
-        jl.ParentJobId = parent_id;
-        jl.AddDate = Utils.getCurrentEasternTime();
-        jl.StartDate = null;
-        jl.EndDate = null;
-        jl.StatusCd = "QUEUED";
-        jl.Year = year;
-        jl.Quarter = quarter;
-        jl.StateCode = stateCd;
-        jl.DataType = dataType;
-        jl.SubType = subType;
-        jl.Url = url;
-        jl.FileName = fileName;
-        jl.ProgramCode = program_code;
-        jl.JobName = name;
-        await this.BulkFileQueue.AddAsync(jl);
-        await this.SaveChangesAsync();
-      }
-      catch (Exception e)
-      {
-        throw new Exception(e.Message);
-      }
-    }
+        private readonly ILogger _logger;
+        public IConfiguration Configuration { get; }
 
-    public async Task<List<ProgramCode>> getProgramCodes()
-    {
-      return await this.ProgramCodes.ToListAsync();
-    }
+        public DbSet<BulkFileMetadata> BulkFileMetadataSet { get; set; }
+        public DbSet<BulkFileQueue> BulkFileQueue { get; set; }
+        public DbSet<JobConfiguration> JobConfigurations { get; set; }
+        public DbSet<JobLog> JobLogs { get; set; }
+        public DbSet<ReportingPeriod> ReportingPeriods { get; set; }
+        public DbSet<BulkFileLog> BulkFileLogs { get; set; }
+        public DbSet<InventoryStatusLog> InventoryStatusLogs { get; set; }
 
-    public async Task<List<List<Object>>> ExecuteSqlQuery(string commandText, int columns)
-    {
+        public DbSet<SubmissionSet> SubmissionSet { get; set; }
 
-      var connectionString = this.Database.GetConnectionString();
-      List<List<Object>> rows = new List<List<Object>>();
+        public DbSet<EmailToProcess> EmailToProcessQueue { get; set; }
+        public DbSet<EmailToSend> EmailToSend { get; set; }
+        public DbSet<ProgramCode> ProgramCodes { get; set; }
+        //public DbSet<EmissionEvaluation> EmissionEvaluations { get; set; } 
+        public DbSet<EvaluationSet> EvaluationSet { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<SeverityCode> SeverityCodes { get; set; }
+        public DbSet<EmissionEvaluation> EmissionEvaluations { get; set; }
+        public DbSet<EvalStatusCode> EvalStatusCodes { get; set; }
+        public DbSet<CheckSession> CheckSessions { get; set; }
+        public DbSet<CorsOptions> CorsOptions { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<MonitorPlan> MonitorPlans { get; set; }
 
-      try
-      {
-        using (var connection = new NpgsqlConnection(connectionString))
+        public DbSet<CertEvent> CertEvents { get; set; }
+
+        public DbSet<TestSummary> TestSummaries { get; set; }
+
+        public DbSet<TestExtensionExemption> TestExtensionExemptions { get; set; }
+        public DbSet<MonitorLocation> MonitorLocations { get; set; }
+        public DbSet<MonitorPlanLocation> MonitorPlanLocations { get; set; }
+
+        public DbSet<PdemReport> PdemReport { get; set; }
+
+        public NpgSqlContext(IConfiguration configuration, ILogger<NpgSqlContext> logger, DbContextOptions<NpgSqlContext> options) : base(options)
         {
-          if (connection.State != ConnectionState.Open)
-            connection.Open();
-
-          await using var cmd = new NpgsqlCommand(commandText, connection);
-          await using var reader = await cmd.ExecuteReaderAsync();
-
-          while (reader.Read())
-          {
-            List<Object> row = new List<Object>();
-
-            for (int i = 0; i < columns; i++)
+            _logger = logger;
+            Configuration = configuration;
+        }
+        public async Task CreateBulkFileRecord(string name, Guid parent_id, int? year, int? quarter, string stateCd, string dataType, string subType, string url, string fileName, Guid job_id, string program_code)
+        {
+            try
             {
-              row.Add(reader.GetProviderSpecificValue(i));
+                Guid child_job_id = Guid.NewGuid();
+                BulkFileQueue jl = new BulkFileQueue();
+                jl.JobId = child_job_id;
+                jl.ParentJobId = parent_id;
+                jl.AddDate = Utils.getCurrentEasternTime();
+                jl.StartDate = null;
+                jl.EndDate = null;
+                jl.StatusCd = "QUEUED";
+                jl.Year = year;
+                jl.Quarter = quarter;
+                jl.StateCode = stateCd;
+                jl.DataType = dataType;
+                jl.SubType = subType;
+                jl.Url = url;
+                jl.FileName = fileName;
+                jl.ProgramCode = program_code;
+                jl.JobName = name;
+                await this.BulkFileQueue.AddAsync(jl);
+                await this.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<List<ProgramCode>> getProgramCodes()
+        {
+            return await this.ProgramCodes.ToListAsync();
+        }
+
+        public async Task<List<List<Object>>> ExecuteSqlQuery(string commandText, int columns)
+        {
+
+            var connectionString = this.Database.GetConnectionString();
+            List<List<Object>> rows = new List<List<Object>>();
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    await using var cmd = new NpgsqlCommand(commandText, connection);
+                    await using var reader = await cmd.ExecuteReaderAsync();
+
+                    while (reader.Read())
+                    {
+                        List<Object> row = new List<Object>();
+
+                        for (int i = 0; i < columns; i++)
+                        {
+                            row.Add(reader.GetProviderSpecificValue(i));
+                        }
+
+                        rows.Add(row);
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
-            rows.Add(row);
-          }
-
-          connection.Close();
+            return rows;
         }
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e);
-      }
 
-      return rows;
-    }
+        public async Task ExecuteEmissionRefreshProcedure(string monPlanId, decimal year, decimal quarter)
+        {
+            var connectionString = this.Database.GetConnectionString();
+            var connection = new NpgsqlConnection(connectionString);
 
-    public async Task ExecuteEmissionRefreshProcedure(string monPlanId, decimal year, decimal quarter)
-    {
-      var connectionString = this.Database.GetConnectionString();
-      var connection = new NpgsqlConnection(connectionString);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
 
-      if (connection.State != ConnectionState.Open)
-        connection.Open();
-
-      await using var cmd = new NpgsqlCommand("CALL camdecmpswks.refresh_emissions_views($1, $2, $3)", connection)
-      {
-        Parameters =
+            await using var cmd = new NpgsqlCommand("CALL camdecmpswks.refresh_emissions_views($1, $2, $3)", connection)
+            {
+                Parameters =
           {
               new() { Value = monPlanId },
               new() { Value = year },
               new() { Value = quarter }
           }
-      };
+            };
 
-      await cmd.ExecuteNonQueryAsync();
-      connection.Close();
-    }
-
-    public void ExecuteSql(string commandText)
-    {
-      var connectionString = this.Database.GetConnectionString();
-
-
-      Console.Write(commandText);
-      try
-      {
-        using (var connection = new NpgsqlConnection(connectionString))
-        {
-          if (connection.State != ConnectionState.Open)
-            connection.Open();
-
-          using (var cmd = new NpgsqlCommand(commandText, connection))
-          {
-            cmd.ExecuteNonQuery();
-          }
-
-          connection.Close();
+            await cmd.ExecuteNonQueryAsync();
+            connection.Close();
         }
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e);
-      }
+
+        public void ExecuteSql(string commandText)
+        {
+            var connectionString = this.Database.GetConnectionString();
+
+
+            Console.Write(commandText);
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    using (var cmd = new NpgsqlCommand(commandText, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void ExecuteSql(string tableName, string commandText, DataSet ds)
+        {
+            var connectionString = this.Database.GetConnectionString();
+
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = commandText;
+
+                DataTable dt = new DataTable(tableName);
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(command);
+                da.Fill(dt);
+                ds.Tables.Add(dt);
+
+                connection.Close();
+            }
+        }
+
+        public NpgsqlCommand ExecuteProcedure(string name, List<NpgsqlParameter> parameters)
+        {
+            var connectionString = this.Database.GetConnectionString();
+            var connection = new NpgsqlConnection(connectionString);
+
+            var command = ExecuteProcedure(name, parameters, connection);
+
+            connection.Close();
+
+            return command;
+        }
+
+        public NpgsqlCommand ExecuteProcedure(string name, List<NpgsqlParameter> parameters, NpgsqlConnection connection, NpgsqlTransaction transaction = null)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = name;
+            command.CommandType = CommandType.StoredProcedure;
+            if (transaction != null)
+            {
+                command.Transaction = transaction;
+            }
+
+            foreach (var param in parameters)
+            {
+                command.Parameters.Add(param);
+            }
+
+            command.ExecuteNonQuery();
+
+            return command;
+        }
+
+        public NpgsqlParameter CreateParameter(string name, string value, NpgsqlDbType type, ParameterDirection direction)
+        {
+            object oValue = value;
+            if (string.IsNullOrEmpty(value))
+            {
+                oValue = System.DBNull.Value;
+            }
+
+            NpgsqlParameter param = new NpgsqlParameter();
+            param.Value = oValue;
+            param.ParameterName = name;
+            param.NpgsqlDbType = type;
+            param.Direction = direction;
+            return param;
+        }
     }
-
-    public void ExecuteSql(string tableName, string commandText, DataSet ds)
-    {
-      var connectionString = this.Database.GetConnectionString();
-
-      using (var connection = new NpgsqlConnection(connectionString))
-      {
-        if (connection.State != ConnectionState.Open)
-          connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = commandText;
-
-        DataTable dt = new DataTable(tableName);
-        NpgsqlDataAdapter da = new NpgsqlDataAdapter(command);
-        da.Fill(dt);
-        ds.Tables.Add(dt);
-
-        connection.Close();
-      }
-    }
-
-    public NpgsqlCommand ExecuteProcedure(string name, List<NpgsqlParameter> parameters)
-    {
-      var connectionString = this.Database.GetConnectionString();
-      var connection = new NpgsqlConnection(connectionString);
-
-      var command = ExecuteProcedure(name, parameters, connection);
-
-      connection.Close();
-
-      return command;
-    }
-
-    public NpgsqlCommand ExecuteProcedure(string name, List<NpgsqlParameter> parameters, NpgsqlConnection connection, NpgsqlTransaction transaction = null)
-    {
-      if (connection.State != ConnectionState.Open)
-        connection.Open();
-
-      var command = connection.CreateCommand();
-      command.CommandText = name;
-      command.CommandType = CommandType.StoredProcedure;
-      if (transaction != null)
-      {
-        command.Transaction = transaction;
-      }
-
-      foreach (var param in parameters)
-      {
-        command.Parameters.Add(param);
-      }
-
-      command.ExecuteNonQuery();
-
-      return command;
-    }
-
-    public NpgsqlParameter CreateParameter(string name, string value, NpgsqlDbType type, ParameterDirection direction)
-    {
-      object oValue = value;
-      if (string.IsNullOrEmpty(value))
-      {
-        oValue = System.DBNull.Value;
-      }
-
-      NpgsqlParameter param = new NpgsqlParameter();
-      param.Value = oValue;
-      param.ParameterName = name;
-      param.NpgsqlDbType = type;
-      param.Direction = direction;
-      return param;
-    }
-  }
 }
