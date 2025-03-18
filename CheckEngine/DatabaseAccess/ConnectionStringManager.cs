149cb8cbed678b14ef2bcf52e936da6d9e871393
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-03-18 15:34:09 -0400

    Updated default values for db settings

--- CheckEngine/DatabaseAccess/ConnectionStringManager.cs
+++ CheckEngine/DatabaseAccess/ConnectionStringManager.cs
@@ -59,14 +59,14 @@ namespace DatabaseAccess
             connectionLifeTime       = Configuration.GetValue<int>("EASEY_DB_CONNECTION_LIFE_TIME", 1800);
             idleInTransactionSessionTimeout = Configuration.GetValue<int>("EASEY_DB_IDLE_TRANS_SESSION_TIMEOUT", 300000);
 
-            return connectionString = $"Server={host};Port={port};Username={user};Password={password};Database={db};Pooling=true;"
-                    + $"ApplicationName={appName};"
-                    + $"MaxPoolSize={maxConnectionPool};"      // Max connections in pool
-                    + $"ConnectionIdleLifetime={connectionIdleLifetime};" // Close idle connections
-                    + $"Timeout={connectionTimeout};"          // Maximum time (ms) to wait for a new connection before timing out.
-                    + $"CommandTimeout={statementTimeout};"  //Npgsql (Client-side) kills if query (command) takes longer than statementTimeout
-                    + $"ConnectionLifeTime={connectionLifeTime};"  //The total maximum lifetime of connections (in seconds).
-                    + $"Options='-c statement_timeout={statementTimeout}';"; //PostgreSQL (db-side) kills if query takes longer than statementTimeout
+            return connectionString = $"Server={host};Port={port};Username={user};Password={password};Database={db};Pooling=true;";
+                    //+ $"ApplicationName={appName};"
+                    //+ $"MaxPoolSize={maxConnectionPool};"      // Max connections in pool
+                    //+ $"ConnectionIdleLifetime={connectionIdleLifetime};" // Close idle connections
+                    //+ $"Timeout={connectionTimeout};"          // Maximum time (ms) to wait for a new connection before timing out.
+                    //+ $"CommandTimeout={statementTimeout};"  //Npgsql (Client-side) kills if query (command) takes longer than statementTimeout
+                    //+ $"ConnectionLifeTime={connectionLifeTime};"  //The total maximum lifetime of connections (in seconds).
+                    //+ $"Options='-c statement_timeout={statementTimeout}';"; //PostgreSQL (db-side) kills if query takes longer than statementTimeout
         }
     }
 }
