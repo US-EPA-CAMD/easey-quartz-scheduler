6825b294c2fd1d57e941fd52123167d7d247b660
Mark Hayward <mark.hayward@erg.com>
2025-04-25 07:46:14 -0400

    update release branch with logging variable name from dev

--- admin/Program.cs
+++ admin/Program.cs
@@ -12,54 +12,53 @@ namespace Epa.Camd.Quartz.Scheduler
 {
   public class Program
   {
-    public static int Main(string[] args)
-    {
-      try
-      {
-          DotNetEnv.Env.Load(); //Loads from .env if present
+	public static int Main(string[] args) {
+	   try
+	   {
+		  DotNetEnv.Env.Load(); //Loads from .env if present
 
-          var logLevelEnv = Environment.GetEnvironmentVariable("LOGGING__LEVEL") ?? "Information";
-          var parsedLevel = Enum.TryParse(logLevelEnv, true, out LogEventLevel logLevel)
-            ? logLevel
-            : LogEventLevel.Information;
+		  var logLevelEnv = Environment.GetEnvironmentVariable("EASEY_LOG_LEVEL") ?? "Information";
+		  var parsedLevel = Enum.TryParse(logLevelEnv, true, out LogEventLevel logLevel)
+			? logLevel
+			: LogEventLevel.Information;
 
-          Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Is(parsedLevel)
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .WriteTo.Console(new RenderedCompactJsonFormatter())
-            .CreateLogger();
+		  Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Is(parsedLevel)
+			.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+			.Enrich.FromLogContext()
+			.WriteTo.Console(new RenderedCompactJsonFormatter())
+			.CreateLogger();
 
-            Log.Information("Starting web host");
-            CreateHostBuilder(args).Build().Run();
-            return 0;
-      }
-      catch (Exception ex)
-      {
-            Console.WriteLine("FATAL: Logging configuration failed: " + ex.Message);
-            Console.WriteLine(ex);
-        return 1;
-      }
-    }
+		  Log.Information("Starting web host");
+		  CreateHostBuilder(args).Build().Run();
+		  return 0;
+	  }
+	  catch (Exception ex)
+	  {
+		Console.WriteLine("FATAL: Logging configuration failed: " + ex.Message);
+		Console.WriteLine(ex);
+		return 1;
+	  }
+	}
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureLogging((ctx, logging) =>
-              {
-                // Create the factory and wire it to LoggerProvider
-                var factory = LoggerFactory.Create(builder =>
-                {
-                  builder.ClearProviders();
-                  builder.AddSerilog(); // use the Serilog pipeline already built
-                });
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+			.ConfigureLogging((ctx, logging) =>
+			  {
+				// Create the factory and wire it to LoggerProvider
+				var factory = LoggerFactory.Create(builder =>
+				{
+				  builder.ClearProviders();
+				  builder.AddSerilog(); // use the Serilog pipeline already built
+				});
 
-                Epa.Camd.Logger.LoggerProvider.Configure(factory);
-            })
-            .UseSerilog() // required to wire Serilog to Microsoft.Extensions.Logging
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            })
-            .ConfigureSilkierQuartzHost();
+				Epa.Camd.Logger.LoggerProvider.Configure(factory);
+			})
+			.UseSerilog() // required to wire Serilog to Microsoft.Extensions.Logging
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+			  webBuilder.UseStartup<Startup>();
+			})
+			.ConfigureSilkierQuartzHost();
   }
 }
