098e66713083d8bf539b601308ce11a087f1ed7a
ergjustin <justin.joseph.erg.com>
2025-05-15 15:19:26 -0500

    AdditionalDetails column with list of programs generated

--- admin/Jobs/BulkDataFiles/AllowanceComplianceBulkDataFiles.cs
+++ admin/Jobs/BulkDataFiles/AllowanceComplianceBulkDataFiles.cs
@@ -45,7 +45,8 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
 
       _logger.LogInformation("Creating Allowance Compliance JobLog. JobId: {JobId}", job_id);
 
-      JobLog jl = new JobLog(); 
+      JobLog jl = new JobLog();
+      List<string> generatedPrograms = new List<string>();
 
       try
       {
@@ -61,23 +62,26 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
 
         _dbContext.JobLogs.Add(jl);
         await _dbContext.SaveChangesAsync();
-        
-        List<List<Object>> rowsPerPrg = await _dbContext.ExecuteSqlQuery("SELECT * FROM camdaux.vw_allowance_based_compliance_bulk_files_to_generate", 1);
+
+        List<List<Object>> distinctPrograms = await _dbContext.ExecuteSqlQuery("SELECT DISTINCT prg_code FROM camdaux.vw_allowance_based_compliance_bulk_files_to_generate", 1);
 
 
-        for(int row = 0; row < rowsPerPrg.Count; row++){
-          string code = (string) rowsPerPrg[row][0];
+        foreach(var programRow in distinctPrograms){
+          string code = (string)programRow[0];
           string urlParams = "programCodeInfo=" + code;
 
-          await _dbContext.CreateBulkFileRecord("Allowance-Compliance-"+code, job_id, null, null, null, "Compliance", null, Utils.Configuration["EASEY_STREAMING_SERVICES"] + "/allowance-compliance?" + urlParams, "compliance/compliance-" + code.ToLower() + ".csv", job_id, code);
+          await _dbContext.CreateBulkFileRecord("Allowance-Compliance-" + code, job_id, null, null, null, "Compliance", null, Utils.Configuration["EASEY_STREAMING_SERVICES"] + "/allowance-compliance?" + urlParams, "compliance/compliance-" + code.ToLower() + ".csv", job_id, code);
+          generatedPrograms.Add(code);
+          _logger.LogInformation("Generated allowance compliance file program {ProgramCode}. JobId: {JobId}", code, job_id);
         }
         
                 
         jl.StatusCd = "COMPLETE";
         jl.EndDate = Utils.getCurrentEasternTime();
+        jl.AdditionalDetails = $"Generated programs: {string.Join(", ", generatedPrograms)}";
         _dbContext.JobLogs.Update(jl);
         await _dbContext.SaveChangesAsync();
-        _logger.LogInformation("AllowanceComplianceBulkDataFiles job completed successfully. JobId: {JobId}", job_id);
+        _logger.LogInformation("AllowanceComplianceBulkDataFiles job completed successfully. Generated files {ProgramCount} programs: {ProgramList}. JobId: {JobId}", generatedPrograms.Count, string.Join(", ", generatedPrograms), job_id);
       }
       catch (Exception e)
       {
