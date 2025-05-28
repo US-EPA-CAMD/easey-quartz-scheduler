578a7946312c9f1398b942c777160b1a01ee8706
Jon Maxwell Diebold <max.diebold@erg.com>
2025-05-28 17:33:14 -0400

    fix(6682): workaround for 2 new compile warnings

--- admin/Jobs/EmailQueueJobs/SubmissionReminderQueue.cs
+++ admin/Jobs/EmailQueueJobs/SubmissionReminderQueue.cs
@@ -119,6 +119,7 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
         }
         _dbContext.SaveChanges();
 
+        await Task.Yield(); // TODO: Remove when the API call is implemented
         return;
       }
       catch (Exception e)
