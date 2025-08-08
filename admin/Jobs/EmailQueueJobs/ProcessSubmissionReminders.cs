ec03df405c1925430f35c6529008aaacb020e1f3
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-08-08 11:20:14 -0400

    Now registers with quartz for dynamic scheduling

--- admin/Jobs/EmailQueueJobs/ProcessSubmissionReminders.cs
+++ admin/Jobs/EmailQueueJobs/ProcessSubmissionReminders.cs
@@ -4,14 +4,30 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Quartz;
 using Epa.Camd.Quartz.Scheduler.Models;
+using Microsoft.Extensions.DependencyInjection;
+using SilkierQuartz;
 
 namespace Epa.Camd.Quartz.Scheduler.Jobs
 {
     public class ProcessSubmissionReminders : BaseEmailProcessor, IJob
     {
+        private static readonly string GROUP = Constants.QuartzGroups.MAINTAINANCE;
+        private static readonly string JOB_NAME = "ProcessSubmissionReminders";
+        private static readonly string JOB_DESCRIPTION = "Sends submission reminder emails.";
+        
         public ProcessSubmissionReminders(NpgSqlContext dbContext, IConfiguration configuration, ILogger<ProcessSubmissionReminders> logger)
             : base(dbContext, configuration, logger)
         {
+        }
+        
+        public static void RegisterWithQuartz(IServiceCollection services)
+        {
+            services.AddQuartzJob<ProcessSubmissionReminders>(WithJobKey(), JOB_DESCRIPTION);
+        }
+        
+        private static JobKey WithJobKey()
+        {
+            return new JobKey(JOB_NAME, GROUP);
         }
 
         protected override string GetEmailTypeForRecipientApi()
@@ -26,7 +42,7 @@ namespace Epa.Camd.Quartz.Scheduler.Jobs
 
         protected override string GetJobName()
         {
-            return "ProcessSubmissionReminders";
+            return JOB_NAME;
         }
 
         public async Task Execute(IJobExecutionContext context)
